274068913ccb70e1f66ecf483ddf4d6b5e044a1b
Esteban Magoja <estebanmagoja@hotmail.com>
2026-03-13 12:19:01 -0300

    logo bienesonline

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://raxta.com/storage/auth/logo.PNG" alt="Logo" {{ $attributes->merge(['class' => 'h-8']) }}>
+<img src="https://bienesonline.ai/storage/auth/logo.png" alt="BienesOnLine" {{ $attributes->merge(['class' => 'h-8']) }}>
