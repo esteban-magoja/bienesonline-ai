3ed86b9282ed459defe385b1ed38d7aaa0ab1626
Esteban Magoja <estebanmagoja@hotmail.com>
2026-08-04 14:44:05 -0300

    evitar cargar analitycs para bots

--- resources/themes/anchor/partials/footer-scripts.blade.php
+++ resources/themes/anchor/partials/footer-scripts.blade.php
@@ -6,7 +6,12 @@
 
 {{-- @yield('javascript') --}}
 
-@if(setting('site.google_analytics_tracking_id', ''))
+@php
+    $userAgent = request()->userAgent() ?? '';
+    $isBot = preg_match('/(?:bot|crawler|spider|slurp|facebookexternalhit|facebot|twitterbot|linkedinbot|whatsapp|telegrambot|slackbot|discordbot|bingpreview|headlesschrome)/i', $userAgent) === 1;
+@endphp
+
+@if(setting('site.google_analytics_tracking_id', '') && ! $isBot)
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={{ setting('site.google_analytics_tracking_id') }}"></script>
     <script>
